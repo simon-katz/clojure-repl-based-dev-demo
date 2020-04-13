462766fb705fce82f203cc2d0f03f1d5be9ba23e
Simon Katz <sk@nomistech.com>
2020-04-13 18:09:45 +0100

    Simplify requires of the standard REPL utils

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,15 +1,21 @@
 (ns dev
-  (:require [clojure.java.javadoc :refer [javadoc]]
-            [clojure.pprint :refer [pp pprint]]
-            [clojure.repl :refer [apropos dir doc find-doc pst source]]
+  (:require [clojure.main]
             [clojure.tools.namespace.repl :as tnr]))
 
 ;;;; ___________________________________________________________________________
+;;;; Require the standard REPL utils.
+;;;;
+;;;; This is useful in a `dev` namespace, and is needed in a `user` namespace
+;;;; because the requires get blatted by `tnr/refresh` and `reset`.
 
-;;;; `clojure.tools.namespace.repl` sometimes doesn't work, because it doesn't
-;;;; know what directories to read from when refreshing. This is a problem
-;;;; caused by different behaviour around classpaths on different versions of
-;;;; the JVM.
+(apply require clojure.main/repl-requires)
+
+;;;; ___________________________________________________________________________
+
+;;;; `clojure.tools.namespace.repl/refresh` sometimes doesn't work, because it
+;;;; doesn't know what directories to read from when refreshing. This is a
+;;;; problem caused by different behaviour around classpaths on different
+;;;; versions of the JVM.
 ;;;;
 ;;;; See:
 ;;;; - https://ask.clojure.org/index.php/8288/java-11-and-tools-namespace
